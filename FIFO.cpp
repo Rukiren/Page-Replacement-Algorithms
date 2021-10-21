044ad94282e8d89902b93a19b32192cc6466c19c
Rukiren <Rukiren2004@gmail.com>
2021-10-21 10:01:11 +0800

    2021/10/21

--- FIFO.cpp
+++ FIFO.cpp
@@ -5,15 +5,16 @@ int in[3];
 
 int main(){
     int n;
-    cin >> n;
+    cin >> n;   // 總共有多少數字要存入
     int time = 0;
     for (int i=0;i<n;i++){
-      if (n == 3){
-        n = 0;
+      if (time == 3){
+          time = 0;   // 要存入哪個記憶體
       }
       int temp;
       cin >> temp;
-      in[n] = temp;
-      n++;
+      in[time] = temp;
+      time++;   // 變成下個記憶體
     }
+    // 此處只有寫邏輯，沒有寫輸出。
 }
