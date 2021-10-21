11cefd65fa433932bd59802bf3326fed353a0961
Rukiren <Rukiren2004@gmail.com>
2021-10-21 10:36:12 +0800

    2021/10/21

--- FIFO.cpp
+++ FIFO.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int in[3];
+int in[3]; // 記憶體大小
 
 int main(){
     int n;
--- LFU.cpp
+++ LFU.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 100000;
+
+int time_all[MAX] = {0};
 
 int main(){
   int n;
-  cin >> n;
+  cin >> n; // 輸入總量
   
 }
